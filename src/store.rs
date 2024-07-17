eae892206d6cf17ba0be55d3a5e3b6e51da23e16
oka311119 <katoko.318@gmail.com>
2024-07-17 20:00:26 +0900

    temp

--- src/store.rs
+++ src/store.rs
@@ -1,13 +1,7 @@
 use crate::model::*;
 
-pub trait EventStore {
-    fn new() -> Self;
-    fn save(&self, event: NoteStateEvent) -> Result<(), DomainError>;
-    fn replay(&self) -> Result<NoteState, DomainError>;
-}
-
 pub struct NoteEventStore {}
-impl EventStore for NoteEventStore {
+impl NoteEventStore {
     fn new() -> Self {
         Self {}
     }
